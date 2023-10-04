6debf2ba5c7805ea39a716e8a8dc0a4caa18e2ed
danilRomin <dvromin@yandex.ru>
2023-10-04 16:20:01 +0700

    loader

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const body = document.querySelector("body")
 
 // Лоадер
-// window.addEventListener("load", function () {
-// const loader = document.querySelector(".loader-wrapper")
-// loader.style.display = "none"
-// })
+window.addEventListener("load", function () {
+const loader = document.querySelector(".loader-wrapper")
+loader.style.display = "none"
+})
 
 // Кроссбраузерная ширина скроллбара
 function getWidth() {
